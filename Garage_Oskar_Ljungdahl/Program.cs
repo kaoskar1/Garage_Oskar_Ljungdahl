c6551f3d2d0cc7b2470f6e5c8dcc4a4c62ebf2eb
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-04-27 15:27:20 +0200

    Add project files.

---
+++ Garage_Oskar_Ljungdahl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_Oskar_Ljungdahl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
